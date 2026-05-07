e626632c813b436dbd9e9a7ee07dcd425931d99c
Manasse Vita <157376426+MVita11@users.noreply.github.com>
2026-05-07 15:01:01 +0100

    Adds image and link splitting for markdown parsing
    Introduces functions to split nodes containing markdown image and link syntax into separate text and media/link nodes, enabling more granular processing of mixed content. Adds corresponding unit test to verify image node splitting logic.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, split_nodes_image
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD, url=None)
@@ -67,6 +67,24 @@ class TestTextNode(unittest.TestCase):
             ("Google", "https://www.google.com"),
             
         ], matches)
+        
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode(
+                "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+            ),
+        ],
+        new_nodes,
+    )
         
 if __name__ == "__main__":
     unittest.main()
